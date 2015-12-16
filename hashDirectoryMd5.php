9b60f2e5afb881fb830b5d05666345104890b495
elwadi amine <elwadiamine@gmail.com>
2015-12-16 13:07:08 +0000

    Create hashDirectoryMd5.php

---
+++ hashDirectoryMd5.php
@@ -0,0 +1,35 @@
+/**
+ * Generate an MD5 hash string from the contents of a directory.
+ *
+ * @param string $directory
+ * @return boolean|string
+ */
+function hashDirectory($directory)
+{
+    if (! is_dir($directory))
+    {
+        return false;
+    }
+ 
+    $files = array();
+    $dir = dir($directory);
+ 
+    while (false !== ($file = $dir->read()))
+    {
+        if ($file != '.' and $file != '..')
+        {
+            if (is_dir($directory . '/' . $file))
+            {
+                $files[] = hashDirectory($directory . '/' . $file);
+            }
+            else
+            {
+                $files[] = md5_file($directory . '/' . $file);
+            }
+        }
+    }
+ 
+    $dir->close();
+ 
+    return md5(implode('', $files));
+}
